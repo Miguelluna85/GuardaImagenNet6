739cf3facb6049c4baa041880a31bd574a90ba3f
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-07-27 12:16:38 -0600

    agrego el contexto de la bdd

---
+++ GuardaImagenNet6/Models/Contexto/PruebasDBContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using GuardaImagenNet6.Models;
+
+namespace GuardaImagenNet6.Models.Contexto
+{
+    public partial class PruebasDBContext : DbContext
+    {
+        public PruebasDBContext()
+        {
+        }
+
+        public PruebasDBContext(DbContextOptions<PruebasDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasIndex(e => e.UserName, "UQ__Usuarios__66DCF95C24125447")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Estatus)
+                    .IsRequired()
+                    .HasColumnName("estatus")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.FechaAlta)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaAlta")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FechaModifico)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaModifico")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.FotoBd).HasColumnName("fotoBD");
+
+                entity.Property(e => e.FotoPath)
+                    .HasMaxLength(3000)
+                    .IsUnicode(false)
+                    .HasColumnName("fotoPath");
+
+                entity.Property(e => e.IdUsuarioAlta).HasColumnName("idUsuarioAlta");
+
+                entity.Property(e => e.IdUsuarioModifico).HasColumnName("idUsuarioModifico");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasColumnName("userName");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ GuardaImagenNet6/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuardaImagenNet6.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? FotoPath { get; set; }
+        public bool? Estatus { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public int IdUsuarioAlta { get; set; }
+        public DateTime FechaModifico { get; set; }
+        public int IdUsuarioModifico { get; set; }
+        public byte[]? FotoBd { get; set; }
+    }
+}
