f9cf99e6924883bacc591912d8fbbbaf5b21e29d
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-07-27 14:41:55 -0600

    save user bdd and preview photo user

--- GuardaImagenNet6/wwwroot/js/usuario/PreVisualmagenUser.js
+++ GuardaImagenNet6/wwwroot/js/usuario/PreVisualmagenUser.js
@@ -1,1 +1,14 @@
-﻿
+﻿const imgFile = document.getElementById('FotoByte');
+const imgUser = document.getElementById('imgUser');
+
+
+imgFile.addEventListener("change",() =>{
+
+    const imagenes = imgFile.files;
+
+    if (imagenes || imagenes.length) {
+
+        const imagen = imagenes[0];
+        imgUser.src = URL.createObjectURL(imagen);
+    }
+});
