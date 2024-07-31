5807b5b505b9157fa77a801086e3dc0c234b3a34
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-07-31 15:33:04 -0600

    se agreo campo FotoName al modelo usuarios

--- GuardaImagenNet6/Models/Usuario.cs
+++ GuardaImagenNet6/Models/Usuario.cs
@@ -6,14 +6,15 @@ namespace GuardaImagenNet6.Models
     public partial class Usuario
     {
         public int Id { get; set; }
-        public string UserName { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string? FotoPath { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FotoPath { get; set; }
         public bool? Estatus { get; set; }
         public DateTime FechaAlta { get; set; }
         public int IdUsuarioAlta { get; set; }
         public DateTime FechaModifico { get; set; }
         public int IdUsuarioModifico { get; set; }
-        public byte[]? FotoBd { get; set; }
+        public byte[] FotoBd { get; set; }
+        public string FotoName { get; set; }
     }
 }
