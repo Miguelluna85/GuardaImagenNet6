561de04b6dbe221b132b1e62cb1d1bd2dfbb6c5c
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-08-18 22:46:01 -0600

    se codifica Path Provider y HelperUpload

---
+++ GuardaImagenNet6/Helpers/HelperUploadImage.cs
@@ -0,0 +1,26 @@
+﻿using GuardaImagenNet6.Providers;
+using System.IO;
+
+namespace GuardaImagenNet6.Helpers
+{
+    public class HelperUploadImage
+    {
+        private readonly IWebHostEnvironment env;
+        public HelperUploadImage(IWebHostEnvironment Env)
+        {
+            env = Env;
+        }
+        public async Task<String> UploadImageAsync(IFormFile file, string imageName, string carpeta)
+        {
+            PathProviderFile pathProvider = new PathProviderFile(env);
+           string path= pathProvider.MapPath("", "");
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return path;
+        }
+    }
+}
--- GuardaImagenNet6/Providers/PathProviderFile.cs
+++ GuardaImagenNet6/Providers/PathProviderFile.cs
@@ -2,5 +2,16 @@
 {
     public class PathProviderFile
     {
+        private readonly IWebHostEnvironment env;
+        public PathProviderFile(IWebHostEnvironment Env)
+        {
+                this.env = Env;
+        }
+        public string MapPath(string fileName,string carpetaName )
+        {
+            string path = Path.Combine(this.env.WebRootPath, carpetaName, fileName);
+
+            return path;
+        }
     }
 }
