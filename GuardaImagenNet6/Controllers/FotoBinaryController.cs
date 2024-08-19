5796034d46535ebc9a6ed6e4ba8865378ec91308
Miguelluna85 <85904166+Miguelluna85@users.noreply.github.com>
2024-08-18 19:57:46 -0600

    crearon 2 controladores, Foto para guardar por binario y por ruta

---
+++ GuardaImagenNet6/Controllers/FotoBinaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuardaImagenNet6.Controllers
+{
+    public class FotoBinaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GuardaImagenNet6/Controllers/FotoPathController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuardaImagenNet6.Controllers
+{
+    public class FotoPathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
